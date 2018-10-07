cbf13eaf623291f07fa5e25bbe9b6a23468b7c96
Mehdi Saidi <mehdisaidi879@gmail.com>
2018-10-07 13:32:41 -0400

    I am adding part 5

---
+++ Hand.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Hand {
+public:
+	Hand();
+
+	int getArtillery();
+	int getInfantry();
+	int getCavalery();
+	int getTotalCards();
+	int getArmies();
+
+	void setArtillery(int Artillery);
+	void setInfantry(int Artillery);
+	void setCavalery(int Artillery);
+	void setTotalCards(int totalCards);
+
+	void ArtilleryIncrement(int increment);
+	void InfantryIncrement(int increment);
+	void CavaleryIncrement(int increment);
+	void totalCardsIncrement(int increment);
+	
+	void exchange(string type);
+private:
+	int artillery = 0;
+	int infantry = 0;
+	int cavalery = 0;
+	int totalCards = 0;
+	int armies = 0;
+	int exchangeCounter = 0;
+};
