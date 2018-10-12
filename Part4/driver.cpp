8f52d5f85b796447eb6802397d6e89ae2c410b24
Yashar <yashardabiran75@gmail.com>
2018-10-12 16:34:47 -0400

    Fixed error with part 4 main

--- Part4/driver.cpp
+++ Part4/driver.cpp
@@ -8,10 +8,6 @@ int main()
     Country * c2 = new Country("c2") ;
     Country * c3 = new Country("c3") ;
     std::vector<Country*> c = {c1,c2, c3};
-    Country * c1;
-    Country * c2 ;
-    Country * c3 ;
-    std::vector<Country*> c = {c1,c2, c3};
     Player p1;
     p1.setCountries(c);
    /* p1.reinforce();*/
