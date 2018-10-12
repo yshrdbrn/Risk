cc654b77764d9d737021e32bbf5cd283c3a86569
Yashar <yashardabiran75@gmail.com>
2018-10-12 15:38:31 -0400

    Added executable for part 4

--- Part4/driver.cpp
+++ Part4/driver.cpp
@@ -6,7 +6,7 @@ int main()
     Country * c1;
     Country * c2 ;
     Country * c3 ;
-    std:vector<Country*> c = {c1,c2, c3};
+    std::vector<Country*> c = {c1,c2, c3};
     Player p1;
     p1.setCountries(c);
    /* p1.reinforce();*/
