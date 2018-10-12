317c8006a276d561d7c9b6d6c8cac2a19e32dba5
Youssef Sbeiti <Sbeity97@gmail.com>
2018-10-12 15:16:03 -0400

    Added part 4

---
+++ Part4/Player.h
@@ -0,0 +1,36 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <vector>
+#include <string>
+#include "../Part1/country.h"
+#include "../part_5/Hand.h"
+/*#include "Dice.h"*/
+
+class Player{
+    private:
+
+     
+        std::vector<Country*> countries;
+        /*Hand hand;
+        /*Dice dice;*/
+
+    public:
+        Player();
+        Hand getHand();
+        void setHand(Hand hand);
+
+       /* Dice getDice();
+        void setDice(Dice d);*/
+
+        void setCountries(std::vector<Country*> c);
+
+        std::vector<Country *> getCountries();
+
+        void reinforce();
+        void attack();
+        void fortify();
+
+};
+
+#endif
---
+++ Part4/driver.cpp
@@ -0,0 +1,19 @@
+#include "Player.h"
+#include "../Part1/country.h"
+
+int main()
+{
+    Country * c1;
+    Country * c2 ;
+    Country * c3 ;
+    std:vector<Country*> c = {c1,c2, c3};
+    Player p1;
+    p1.setCountries(c);
+   /* p1.reinforce();*/
+
+
+   /* Hand h;
+    p1.setHand(h);*/
+    return 0;
+}
+
