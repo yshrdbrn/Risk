f34cd2ddc13e674deb4e93cd7b15ed4e37d800e4
Yashar <yashardabiran75@gmail.com>
2018-11-29 15:45:11 -0500

    Implemented user interactions for tournament

--- View/GameSetupView.cpp
+++ View/GameSetupView.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <exception>
 #include "GameSetupView.h"
 
 int GameSetupView::promptUserToChooseMapFile(std::vector<std::string> fileNames) {
@@ -46,21 +48,96 @@ void GameSetupView::showError(std::string error) {
 }
 
 int GameSetupView::chooseNumberOfMaps() {
-    // TODO
-    return 0;
+    int numberOfMaps = 0;
+    while (true) {
+        std::cout << "Enter number of maps (between 1 and 5):";
+        std::cin >> numberOfMaps;
+        if (numberOfMaps >= 1 && numberOfMaps <= 5)
+            break;
+
+        // If number not valid
+        std::cout << "Number not valid." << "\n\n";
+    }
+
+    return numberOfMaps;
 }
 
 int GameSetupView::chooseNumberOfGames() {
-    // TODO
-    return 0;
+    int numberOfGames = 0;
+    while (true) {
+        std::cout << "Enter number of games (between 1 and 5):";
+        std::cin >> numberOfGames;
+        if (numberOfGames >= 1 && numberOfGames <= 5)
+            break;
+
+        // If number not valid
+        std::cout << "Number not valid." << "\n\n";
+    }
+
+    return numberOfGames;
 }
 
 int GameSetupView::chooseMaximumNumberOfTurns() {
-    // TODO
-    return 0;
+    int numberOfTurns = 0;
+    while (true) {
+        std::cout << "Enter number of turns (between 10 and 50):";
+        std::cin >> numberOfTurns;
+        if (numberOfTurns >= 10 && numberOfTurns <= 50)
+            break;
+
+        // If number not valid
+        std::cout << "Number not valid." << "\n\n";
+    }
+
+    return numberOfTurns;
 }
 
 std::vector<std::string> GameSetupView::getPlayerTypes() {
-    // TODO
-    return std::vector<std::string>();
+    int numberOfPlayers = 0;
+    while (true) {
+        std::cout << "Enter number of maps (between 2 and 4):";
+        std::cin >> numberOfPlayers;
+        if (numberOfPlayers >= 2 && numberOfPlayers <= 4)
+            break;
+
+        // If number not valid
+        std::cout << "Number not valid." << "\n\n";
+    }
+
+    std::vector<std::string> playerTypes;
+    for (int i = 0; i < numberOfPlayers; i++) {
+        std::cout << "Enter the type number of player " + std::to_string(i + 1) + ": " << std::endl;
+        std::cout << "\t1) Aggressive" << std::endl;
+        std::cout << "\t2) Benevolent" << std::endl;
+        std::cout << "\t3) Random" << std::endl;
+        std::cout << "\t4) Cheater" << std::endl;
+
+        int choice;
+        while (true) {
+            std::cin >> choice;
+            if (choice >= 1 && choice <= 4)
+                break;
+            // If number not valid
+            std::cout << "Number not valid." << "\n\n";
+        }
+
+        switch (choice) {
+            case 1:
+                playerTypes.emplace_back("attacker");
+                break;
+            case 2:
+                playerTypes.emplace_back("defender");
+                break;
+            case 3:
+                playerTypes.emplace_back("random");
+                break;
+            case 4:
+                playerTypes.emplace_back("cheater");
+                break;
+            default:
+                throw std::exception();
+        }
+    }
+
+    return playerTypes;
 }
